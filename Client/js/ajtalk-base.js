117a9c50db2ea9d85a6da03b72a828552a17a673
ajlopez <webmaster@ajlopez.com>
2011-10-31 18:42:39 +0000

    Client code, first steps

---
+++ Client/js/ajtalk-base.js
@@ -0,0 +1,74 @@
+base = function() {
+
+var Smalltalk;
+
+Number.prototype._max_ = function(x) {
+	if (this >= x)
+		return this;
+	return x;
+}
+
+Number.prototype._min_ = function(x) {
+	if (this >= x)
+		return this;
+	return x;
+}
+
+Number.prototype['*'] = function(x) {
+	return this * x;
+}
+
+Number.prototype['@'] = function(x) {
+	var point = new Smalltalk.Point();
+	console.log('new Point with ' + this + ' and ' + x);
+	point._setX_setY_(this, x);
+	return point;
+}
+
+Boolean.prototype._ifTrue_ = function(block) {
+	if (this == true)
+		return block();
+	return null;
+}
+
+Boolean.prototype._ifTrue_ifFalse_ = function(blockTrue, blockFalse) {
+	if (this == true)
+		return blockTrue();
+	else
+		return blockFalse();
+}
+
+Boolean.prototype._ifFalse_ = function(block) {
+	if (this == false)
+		return block();
+	return null;
+}
+
+Function.prototype._isBlock = function() { return true; }
+
+Object.prototype._isBlock = function() { return false; }
+
+var send = function(receiver, selector, args)
+{
+	console.log('Sending ' + selector + ' to ' + receiver);
+	if (args == undefined)
+		return receiver[selector].apply(receiver);
+	return receiver[selector].apply(receiver, args);
+}
+
+var sendSuper = function(receiver, clazz, selector, args)
+{
+	return clazz.__super.prototype[selector].apply(receiver, args);
+}
+
+var using = function(st) {
+	Smalltalk = st;
+}
+
+return {
+	send: send,
+	sendSuper: sendSuper,
+	using: using
+}
+
+}();
---
+++ Client/js/ajtalk-primitives.js
@@ -0,0 +1,13 @@
+
+primitives = function() {
+
+var primitive111 = function(self) {
+	return { value: self.__class };
+};
+
+return {
+	primitive111: primitive111
+}
+
+}();
+
