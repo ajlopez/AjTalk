af3c7f2df53a9bc3b1200b055595de2df1e7a8c0
ajlopez <webmaster@ajlopez.com>
2012-11-20 09:41:31 -0300

    BytecodeCompiler Expressions first tests

---
+++ Src/AjTalk.Tests/Compilers/Vm/BytecodeCompilerExpressionsTests.cs
@@ -0,0 +1,31 @@
+﻿namespace AjTalk.Tests.Compilers.Vm
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using AjTalk.Model;
+    using AjTalk.Compilers.Vm;
+    using AjTalk.Language;
+
+    [TestClass]
+    public class BytecodeCompilerExpressionsTests
+    {
+        [TestMethod]
+        [DeploymentItem(@"CodeFiles\Expressions.txt")]
+        public void CompileExpressions()
+        {
+            IList<ExpressionResult> results = ExpressionResult.LoadExpressionResults("Expressions.txt");
+
+            foreach (var result in results)
+            {
+                ModelParser parser = new ModelParser(result.Text);
+                Block block = new Block();
+                BytecodeCompiler compiler = new BytecodeCompiler(block);
+                compiler.CompileExpression(parser.ParseExpression());
+                result.ValidateBlock(block);
+            }
+        }
+    }
+}
