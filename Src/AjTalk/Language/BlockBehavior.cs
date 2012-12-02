b857664370feef5a4f386ed853036ffcc8c201f7
ajlopez <webmaster@ajlopez.com>
2012-12-01 22:02:02 -0300

    Web Server WIP

--- Src/AjTalk/Language/BlockBehavior.cs
+++ Src/AjTalk/Language/BlockBehavior.cs
@@ -16,12 +16,28 @@
             this.DefineInstanceMethod(new FunctionalMethod("value:", this, this.ValueMethod));
             this.DefineInstanceMethod(new FunctionalMethod("value:value:", this, this.ValueMethod));
             this.DefineInstanceMethod(new FunctionalMethod("assert", this, this.AssertMethod));
+            this.DefineInstanceMethod(new FunctionalMethod("whileTrue:", this, this.WhileTrueMethod));
         }
 
         private object ValueMethod(object obj, object[] arguments)
         {
             Block block = (Block)obj;
             return (new ExecutionBlock(this.Machine, null, block, arguments)).Execute();
+        }
+
+        private object WhileTrueMethod(object obj, object[] arguments)
+        {
+            Block block = (Block)obj;
+            Block body = (Block)arguments[0];
+            var result = (new ExecutionBlock(this.Machine, null, block, arguments)).Execute();
+
+            while (result.Equals(true))
+            {
+                body.Execute(this.Machine, arguments);
+                result = (new ExecutionBlock(this.Machine, null, block, arguments)).Execute();
+            }
+
+            return null;
         }
 
         private object AssertMethod(object obj, object[] arguments)
