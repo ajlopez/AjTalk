60f288237c536ef9fabd66fefc05cd637330d609
ajlopez <webmaster@ajlopez.com>
2012-12-12 08:13:02 -0300

    Process with two tests

---
+++ Src/AjTalk.Tests/Language/ProcessTests.cs
@@ -0,0 +1,44 @@
+﻿namespace AjTalk.Tests.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjTalk.Language;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Threading;
+    using AjTalk.Compiler;
+
+    [TestClass]
+    public class ProcessTests
+    {
+        [TestMethod]
+        public void NewProcess()
+        {
+            Machine machine = new Machine();
+            Block block = new Block();
+
+            Process process = new Process(block, null, machine);
+
+            Assert.AreSame(machine, process.Machine);
+            Assert.AreSame(block, process.Block);
+            Assert.IsNull(process.Arguments);
+        }
+
+        [TestMethod]
+        public void RunProcess()
+        {
+            AutoResetEvent handle = new AutoResetEvent(false);
+            Machine machine = new Machine();
+            machine.SetGlobalObject("handle", handle);
+            Block block = (new VmCompiler()).CompileBlock("handle !Set");
+
+            Process process = new Process(block, null, machine);
+
+            process.Start();
+
+            if (!handle.WaitOne(500))
+                Assert.Fail("Process didn't run");
+        }
+    }
+}
---
+++ Src/AjTalk/Language/Process.cs
@@ -0,0 +1,40 @@
+﻿namespace AjTalk.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+
+    public class Process
+    {
+        private IBlock block;
+        private object[] arguments;
+        private Machine machine;
+
+        public Process(IBlock block, object[] arguments, Machine machine)
+        {
+            this.block = block;
+            this.arguments = arguments;
+            this.machine = machine;
+        }
+
+        public IBlock Block { get { return this.block; } }
+
+        public object[] Arguments { get { return this.arguments; } }
+
+        public Machine Machine { get { return this.machine; } }
+
+        public void Start()
+        {
+            ThreadStart start = new ThreadStart(this.Run);
+            Thread thread = new Thread(start);
+            thread.Start();
+        }
+
+        private void Run()
+        {
+            this.block.Execute(this.machine, this.arguments);
+        }
+    }
+}
