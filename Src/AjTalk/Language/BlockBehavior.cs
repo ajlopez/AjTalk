aed5508cc6c9044fea6c211ea6a00f4062808195
ajlopez <webmaster@ajlopez.com>
2012-12-16 09:21:01 -0300

    Removed while true, while false native methods from Block Behavior

--- Src/AjTalk/Language/BlockBehavior.cs
+++ Src/AjTalk/Language/BlockBehavior.cs
@@ -13,42 +13,6 @@
             : base(behavior, superclass, machine, typeof(Block))
         {
             this.DefineInstanceMethod(new FunctionalMethod("assert", this, this.AssertMethod));
-            this.DefineInstanceMethod(new FunctionalMethod("whileTrue:", this, this.WhileTrueMethod));
-            this.DefineInstanceMethod(new FunctionalMethod("whileFalse:", this, this.WhileFalseMethod));
-        }
-
-        private object WhileTrueMethod(Machine machine, object obj, object[] arguments)
-        {
-            Block block = (Block)obj;
-            Block body = (Block)arguments[0];
-            var result = block.Execute(machine, null);
-
-            while (result.Equals(true))
-            {
-                body.Execute(this.Machine, null);
-                if (body.Closure != null && body.Closure.HasReturnValue)
-                    return null;
-                result = block.Execute(machine, null);
-            }
-
-            return null;
-        }
-
-        private object WhileFalseMethod(Machine machine, object obj, object[] arguments)
-        {
-            Block block = (Block)obj;
-            Block body = (Block)arguments[0];
-            var result = block.Execute(machine, null);
-
-            while (result.Equals(false))
-            {
-                body.Execute(this.Machine, null);
-                if (body.Closure != null && body.Closure.HasReturnValue)
-                    return null;
-                result = block.Execute(machine, null);
-            }
-
-            return null;
         }
 
         private object AssertMethod(Machine machine, object obj, object[] arguments)
